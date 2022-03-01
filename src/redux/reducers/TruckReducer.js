ce6f19e2ccf6bdd063e22f6bdb415cd06e7d7215
luism007 <luisgm.w001@gmail.com>
2022-03-01 09:31:13 -0700

    Fixed a bug in the TruckReducer.

--- src/redux/reducers/TruckReducer.js
+++ src/redux/reducers/TruckReducer.js
@@ -1,10 +1,13 @@
 // state initialized to an empty array, since we'll be storing an array of trucks
-const truckReducer = (state = {}, action) => {
+const truckReducer = (state = [], action) => {
     switch(action.type) { 
         case "CREATE_TRUCK":
             return [...state, action.truck];
         case "INITIALIZE_TRUCKS":
-            return [state, ...action.trucks];
+            return [
+                ...state,
+                ...action.trucks
+            ];
         default:
             return state;
     }
