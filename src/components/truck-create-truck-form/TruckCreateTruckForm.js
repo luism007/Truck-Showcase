92daede27bd1ef0b1653e67557ba8aae4ecc3462
luism007 <luisgm.w001@gmail.com>
2022-04-02 22:26:50 -0700

    Added commit.

--- src/components/truck-create-truck-form/TruckCreateTruckForm.js
+++ src/components/truck-create-truck-form/TruckCreateTruckForm.js
@@ -5,7 +5,7 @@ import { connect } from "react-redux";
 import "./TruckCreateTruckForm.css";
 
 const TruckCreateTruckForm = (props) => {
-  const [truck, setTruck] = useState({});
+  const [truck , setTruck] = useState({});
 
   const handleSubmit = (event) => {
     event.preventDefault();
