82d4f4e81815c70145f341b61c26788a41b835d8
luism007 <luisgm.w001@gmail.com>
2022-04-16 17:57:48 -0700

    Divided form and its individual input fields as separate components. Allowing for cleaner code.

---
+++ src/components/truck-input-field/TruckInputField.js
@@ -0,0 +1,12 @@
+const TruckInputField = (props) => {
+    
+    return (
+      <div className="inputContainer">
+        <label>{ props.label }</label>
+        <input type={props.type} required = {props.required} name = {props.name} value = {props.value} onChange={ props.onChange }></input>
+        <span>{props.error}</span>
+      </div>
+    );
+}
+
+export default TruckInputField;
