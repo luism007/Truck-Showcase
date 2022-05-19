f2f78bf958d49308622b75cbc85f45befe83567c
luism007 <luisgm.w001@gmail.com>
2022-05-18 17:16:37 -0700

    Changes to Create Truck Form

--- src/components/truck-input-field/TruckInputField.js
+++ src/components/truck-input-field/TruckInputField.js
@@ -4,9 +4,9 @@ const TruckInputField = (props) => {
     
     return (
       <div className="inputContainer">
-        <label>{ props.label }</label>
-        <input type={props.type} required = {props.required} name = {props.name} value = {props.value} onChange={ props.onChange }></input>
-        <span>{props.error}</span>
+        <label className = "inputLabel">{ props.label }</label>
+        <input className = "inputField" type={props.type} required = {props.required} name = {props.name} value = {props.value} onChange={ props.onChange }></input>
+        <span className = "errorMessage">{props.error}</span>
       </div>
     );
 }
